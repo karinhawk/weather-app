672b274efc1ffc863aca3985ad414a950779423c
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-11-16 14:28:11 +0000

    removed redundant toggle code

--- src/Containers/Forecast/Forecast.jsx
+++ src/Containers/Forecast/Forecast.jsx
@@ -4,15 +4,9 @@ import "./Forecast.scss"
 
 const Forecast = ({ forecast, unit }) => {
 
-    const [dayForecast, setDayForecast] = useState(false);
-
     const forecastArr = forecast.forecastday;
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-
-    const toggleDayForecast = () => {
-        setDayForecast(!dayForecast)
-    }
 
     return (
         <div className="forecast">
@@ -24,7 +18,7 @@ const Forecast = ({ forecast, unit }) => {
                 const avgTemperature = day.day[temp]
                 return (
                     <div>
-                        <div className="forecast__day" onClick={toggleDayForecast}>
+                        <div className="forecast__day">
                             <h2 className="forecast__day__weekday">{dayOfWeek}</h2>
                             <h3 className="forecast__day__temp">{avgTemperature}{unit === "temp_c" ? "°C" : "°F"}</h3>
                             <img className="forecast__day__icon" src={day.day.condition.icon} />
@@ -32,16 +26,6 @@ const Forecast = ({ forecast, unit }) => {
                             <h3 className="forecast__day__hourly">Hourly forecast</h3>
                             </Link>
                         </div>
-                        {dayForecast && <div className="forecast__hours">
-                            {hourArr.map((hour) => {
-                                return (
-                                    <div className="forecast__hour">
-                                        <h3>{hour.temp_c} °C</h3>
-                                        <img src={hour.condition.icon}/>
-                                    </div>
-                                )
-                            })}
-                        </div>}
                     </div>
                 )
             })}
